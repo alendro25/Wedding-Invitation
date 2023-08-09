039ed346c3dbcbae0e27ad25bbcbd5b8304e3a13
alendro25 <alendro25@gmail.com>
2023-08-09 10:56:48 +0700

    Tambahkan backsound

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ btnClose.addEventListener("click", function () {
 offcanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
+
+const myAudio = document.querySelector("#myAudio");
+
+var state = 0;
+function playPause() {
+  if (state == 1) {
+    myAudio.muted = false;
+    state = 0;
+    console.log("Unmuted" + state);
+  } else {
+    myAudio.muted = "true";
+    state = 1;
+    console.log("Muted" + state);
+  }
+  console.log("Kondisi terakhir : " + state);
+}
