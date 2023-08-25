5a4b10fc3daa66e29cd93800edd626dd5db4d81f
alendro25 <alendro25@gmail.com>
2023-08-25 10:23:51 +0700

    Tambahkan fitur kunci scroll page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const stickyTop = document.querySelector(".sticky-top");
 const hamburger = document.querySelector(".navbar-toggler");
 const btnClose = document.querySelector(".btn-close");
 const offcanvas = document.querySelector(".offcanvas");
-
+// JS untuk OFFCANVAS
 hamburger.addEventListener("click", function () {
   stickyTop.style.overflow = "visible";
 });
@@ -14,6 +14,7 @@ offcanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
 
+// JS untuk BG Music
 const myAudio = document.querySelector("#myAudio");
 
 var state = 0;
@@ -29,4 +30,27 @@ function playPause() {
 
 function play() {
   myAudio.play();
+
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = "smooth";
+  localStorage.setItem("opened", "true");
+}
+
+// JS untuk lock scrolling sebelum klik "Buka Undangan"
+const rootElement = document.querySelector(":root");
+
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+
+  rootElement.style.scrollBehavior = "auto";
+}
+
+if (!localStorage.getItem("opened")) {
+  disableScroll();
 }
